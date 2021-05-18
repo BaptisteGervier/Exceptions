0eb6e85d24319833dd6278d99e6bb07e983fe1d3
bgervier2021 <baptiste.gervier2021@campus-eni.fr>
2021-05-18 10:21:15 +0200

    Cours du 18/05 sur les Exceptions

---
+++ src/fr/eni/NbPlaceException.java
@@ -0,0 +1,12 @@
+package fr.eni;
+
+public class NbPlaceException extends Exception{
+
+    public NbPlaceException(String message) {
+        super(message);
+    }
+
+    public String maMethode(){
+        return "D2WM 140 A";
+    }
+}
